af7da38772e365443dd035fe55463a97971b8836
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2022-02-23 14:09:27 +0100

    Finish user profile model object.
    Finished user profile model entity.

--- src/main/java/com/example/demo/domain/userProfile/UserProfile.java
+++ src/main/java/com/example/demo/domain/userProfile/UserProfile.java
@@ -1,24 +1,29 @@
 package com.example.demo.domain.userProfile;
 
+import com.example.demo.domain.appUser.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.UUID;
 
 @Entity()
 @Getter@Setter
 @NoArgsConstructor @AllArgsConstructor
 public class UserProfile {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    private String username;
-    private String email;
-    private String password;
+    private String location;
+    private String profilePictureURL;
+    private LocalDate dateOfBirth;
+    private String biography;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
