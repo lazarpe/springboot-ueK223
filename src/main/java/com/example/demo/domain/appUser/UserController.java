eb0a95a7075a9b9a43026fe06b751d8c08e27623
data-corrupted <leandro.piras@noseryoung.ch>
2022-02-23 16:24:37 +0100

    added saveUser() and addRoleToUser()

--- src/main/java/com/example/demo/domain/appUser/UserController.java
+++ src/main/java/com/example/demo/domain/appUser/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.management.InstanceAlreadyExistsException;
 import java.util.Collection;
 import java.util.UUID;
 
@@ -21,10 +22,19 @@ private final UserService userService;
         return ResponseEntity.ok().body("Hello World");
     }
 
+    @GetMapping("/saveUser/{user}")
+    public User saveUser(@PathVariable User user) throws InstanceAlreadyExistsException {
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/addRoleToUser/{username}&{roleName}")
+    public void addRoleToUser(@PathVariable String username, @PathVariable String roleName) {
+        userService.addRoleToUser(username, roleName);
+    }
 
     @GetMapping("/users")
     public ResponseEntity<Collection<User>> findAll() {
-        return new ResponseEntity<Collection<User>>(userService.findAll(), HttpStatus.OK);
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
 }
