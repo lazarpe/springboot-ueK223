1b3badb8b76f68a559b6fdbb3b5aaa495aad08c8
data-corrupted <leandro.piras@noseryoung.ch>
2022-02-24 11:57:50 +0100

    finished CRUD methods for User - re-ordered mappings - corrected mapping types - tested on postman (GET works, POST/PUT needs authentication which I don't have)

--- src/main/java/com/example/demo/domain/appUser/UserController.java
+++ src/main/java/com/example/demo/domain/appUser/UserController.java
@@ -8,7 +8,9 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.management.InstanceAlreadyExistsException;
+import javax.management.InstanceNotFoundException;
 import java.util.Collection;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController @RequestMapping("/api")
@@ -22,19 +24,29 @@ private final UserService userService;
         return ResponseEntity.ok().body("Hello World");
     }
 
-    @GetMapping("/saveUser/{user}")
-    public User saveUser(@PathVariable User user) throws InstanceAlreadyExistsException {
+    @GetMapping("/user")
+    public ResponseEntity<Collection<User>> findAll() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/user/?username={username}")
+    public User getUser(String username) {
+        return userService.getUser(username);
+    }
+
+    @GetMapping("/user/?id={id}")
+    public Optional<User> findById(@PathVariable UUID id) throws InstanceNotFoundException {
+        return userService.findById(id);
+    }
+
+    @PostMapping("/user")
+    public User saveUser(@RequestBody User user) throws InstanceAlreadyExistsException {
         return userService.saveUser(user);
     }
 
-    @GetMapping("/addRoleToUser/{username}&{roleName}")
+    @PutMapping("/user/?username={username}&roleName={roleName}")
     public void addRoleToUser(@PathVariable String username, @PathVariable String roleName) {
         userService.addRoleToUser(username, roleName);
-    }
-
-    @GetMapping("/users")
-    public ResponseEntity<Collection<User>> findAll() {
-        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
     }
 
 }
