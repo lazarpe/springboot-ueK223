fc94e2dee247169fe0a82b193daeaa1f605656b5
Sashimi4 <sascha.buthelezi@noseryoung.ch>
2022-03-02 10:20:51 +0100

    Add modelMapper gradle dependencies.
    Added Gradle dependencies for modelMapper and created a Bean for autowiring into Service layers.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +16,12 @@ public class DemoApplication {
 	}
 
 	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
 }
