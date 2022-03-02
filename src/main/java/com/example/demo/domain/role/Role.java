8477f5ae8963b7b100c644b838d06a2832508781
data-corrupted <leandro.piras@noseryoung.ch>
2022-03-02 14:39:00 +0100

    removed unused imports

--- src/main/java/com/example/demo/domain/role/Role.java
+++ src/main/java/com/example/demo/domain/role/Role.java
@@ -1,15 +1,10 @@
 package com.example.demo.domain.role;
 
-import com.example.demo.domain.appUser.User;
 import com.example.demo.domain.authority.Authority;
 import lombok.*;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 import org.hibernate.validator.constraints.UniqueElements;
-//import org.hibernate.validator.constraints.UniqueElements;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-//import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
